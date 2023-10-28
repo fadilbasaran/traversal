cd3a80b85242d60be7e5caa3446afe4d73b73687
Fadıl Başaran <59345355+fadilbasaran@users.noreply.github.com>
2023-10-29 00:51:12 +0300

    visual test

--- BusinessLayer/ValidationRules/AboutValidator.cs
+++ BusinessLayer/ValidationRules/AboutValidator.cs
@@ -9,7 +9,7 @@ namespace BusinessLayer.ValidationRules
 		public AboutValidator()
 		{
 			RuleFor(x => x.Description).NotEmpty().WithMessage("Açıklama kısmını boş geçemezsiniz...!");
-			RuleFor(x => x.Image1).NotEmpty().WithMessage("Lütfen bir görsel seçiniz...!");
+			RuleFor(x => x.Image1).NotEmpty().WithMessage("LLütfen bir görsel seçiniz...!");
 			RuleFor(x => x.Description).MinimumLength(50).WithMessage("Lütfen en az 50 karakterlik açıklama  giriniz...!");
 			RuleFor(x => x.Description).MaximumLength(1500).WithMessage("Lütfen açıklamayı kısaltınız...!");
 		}
